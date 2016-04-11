363e165a788ea42b385b76fe56c61094d07559fe
Charles Gaudreau Jackson <gaudreaujacksoncharles@gmail.com>
2016-04-11 17:10:04 -0400

    Final structure and js

--- workshop.js
+++ workshop.js
@@ -1,4 +1,16 @@
 /* keyup event handler */
-$('#the-textarea').keyup(function(e) {
-  console.log(e);
+$('#the-textarea').keyup(function(evt) {
+  var currentLength = evt.target.value.length;
+  var maxLength = document.getElementById("the-textarea").getAttribute("maxlength");
+  var availableLength = maxLength - currentLength;
+  document.getElementById("character-counter").innerHTML = availableLength;
+  if (availableLength/maxLength > .2) {
+    $("#character-counter").css("color","green");
+  }
+  else if (availableLength/maxLength < .2 && availableLength/maxLength > .1) {
+    $("#character-counter").css("color","yellow");
+  }
+  else if (availableLength/maxLength < .1) {
+    $("#character-counter").css("color","red");
+  }
 });
